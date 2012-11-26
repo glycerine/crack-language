0aa176a7dc62f096af977056e1fcb46659f753d3
Jason E. Aten <j.e.aten@gmail.com>
2012-11-26 01:59:28 -0800

    repl additions link

--- model/Construct.cc
+++ model/Construct.cc
@@ -731,7 +731,7 @@ bool Construct::loadBootstrapModules() {
     return true;
 }
 
-namespace {
+namespace model {
     
     // Returns a "brief path" for the filename.  A brief path consists of an 
     // md5 hash of the absolute path of the directory of the file, followed by 
@@ -778,7 +778,7 @@ namespace {
 int Construct::runScript(istream &src, const string &name) {
     
     // get the canonical name for the script
-    string canName = modNameFromFile(name);
+    string canName = model::modNameFromFile(name);
     
     // create the builder and context for the script.
     BuilderPtr builder = rootBuilder->createChildBuilder();
--- model/Construct.h
+++ model/Construct.h
@@ -402,7 +402,7 @@ class Construct : public spug::RCBase, public Options {
 
 };
 
- std::string modNameFromFile(const std::string &filename);
+   std::string modNameFromFile(const std::string &filename);
  
 } // namespace model
 
