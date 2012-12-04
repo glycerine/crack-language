d8a703031e18aee41649d87cffdf78bfd096c3c4
Jason E. Aten <j.e.aten@gmail.com>
2012-12-03 21:38:12 -0800

    one repl check now, inside runScript

--- crack_main.cc
+++ crack_main.cc
@@ -302,7 +302,6 @@ int main(int argc, char **argv) {
             crack.options->optionMap["out"] = "crack_output";
         }
         rc = crack.runScript(cin, "<stdin>");
-        if (crack.options->dropintoRepl) { rc = crack.runRepl(); }
 
     } else {
         // it's the script name - run it.
@@ -314,7 +313,6 @@ int main(int argc, char **argv) {
         else {
             crack.setArgv(argc - optind, &argv[optind]);
             rc = crack.runScript(src, argv[optind]);
-            if (crack.options->dropintoRepl) { rc = crack.runRepl(); }
         }
     }
 
