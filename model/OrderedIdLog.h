0a5957912fc792dd0dfe2d3f77847cbda49d9cff
Jason E. Aten <j.e.aten@gmail.com>
2012-12-22 01:11:02 -0800

    add missing license stuff to top

--- model/OrderedIdLog.h
+++ model/OrderedIdLog.h
@@ -1,3 +1,10 @@
+// Copyright 2012 Jason E. Aten
+// 
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// 
+
 #ifndef _model_OrderedIdLog_h_
 #define _model_OrderedIdLog_h_
 
